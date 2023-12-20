ff9e633a16abe0a8a1ecf0d4aa49cd732abadae5
Roni Palmon <roni.palmon@post.runi.ac.il>
2023-12-20 16:28:47 +0200

    fixed coins

--- Coins.java
+++ Coins.java
@@ -7,6 +7,6 @@ public class Coins {
 		int coins= Integer.parseInt(args[0]);
         int quarters= coins / 25;
         int cents= coins % 25;
-        System.out.println("use " + quarters + " quarters " + "and " + cents + " cents ");
+        System.out.println("Use " + quarters + " quarters " + "and " + cents + " cents");
 	}
 }
